0386219453582352ff0c01db5822ad13936c11e8
Abylay <abyllight@gmail.com>
2022-01-01 14:35:35 +0600

    transition

--- resources/js/components/ServiceCard.vue
+++ resources/js/components/ServiceCard.vue
@@ -12,42 +12,43 @@
                 </div>
             </transition>
         </div>
-        <transition name="slide">
-            <div v-if="service.showDiv" class="absolute z-10 inset-x-0 text-white px-4 pt-6 pb-12 bg-gray-700 rounded-md">
-                <div class="w-full my-1">
-                    <div class="flex justify-between items-center">
-                        <p class="text-2xl mb-8">{{service.title}}</p>
-                        <img src="/images/close.svg" class="w-10 -mt-8 cursor-pointer" @click="closeDiv"/>
-                    </div>
-                    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
-                        <div v-for="company in service.companies">
-                            <div v-if="service.has_only_companies">
-                                <a :href="company.link" target="_blank">
-                                    <div class="flex flex-col justify-center items-center gap-2">
-                                        <img :src="company.logo" class="h-12 rounded"/>
+        <div
+            class="absolute z-10 inset-x-0 text-white px-4 bg-gray-700 rounded-md max-h-0 overflow-y-hidden transition-all duration-300 ease-in-out"
+            :class="service.showDiv ? 'max-h-screen pt-6 pb-12' : 'max-h-0'"
+        >
+            <div class="w-full my-1">
+                <div class="flex justify-between items-center">
+                    <p class="text-2xl mb-8">{{service.title}}</p>
+                    <img src="/images/close.svg" class="w-10 -mt-8 cursor-pointer" @click="closeDiv"/>
+                </div>
+                <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
+                    <div v-for="company in service.companies">
+                        <div v-if="service.has_only_companies">
+                            <a :href="company.link" target="_blank">
+                                <div class="flex flex-col justify-center items-center gap-2">
+                                    <img :src="company.logo" class="h-12 rounded"/>
+                                    <p>{{company.name}}</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div v-else>
+                            <router-link :to="{ name: 'Specialists', params: { id: service.id, c_id: company.id } }">
+                                <div
+                                    v-if="company.specialists"
+                                    class="flex items-center gap-2"
+                                >
+                                    <img :src="company.logo" class="h-16 rounded"/>
+                                    <div>
                                         <p>{{company.name}}</p>
+                                        <p class="text-sm">{{company.specialists.length}} специалистов</p>
                                     </div>
-                                </a>
-                            </div>
-                            <div v-else>
-                                <router-link :to="{ name: 'Specialists', params: { id: service.id, c_id: company.id } }">
-                                    <div
-                                        v-if="company.specialists"
-                                        class="flex items-center gap-2"
-                                    >
-                                        <img :src="company.logo" class="h-16 rounded"/>
-                                        <div>
-                                            <p>{{company.name}}</p>
-                                            <p class="text-sm">{{company.specialists.length}} специалистов</p>
-                                        </div>
-                                    </div>
-                                </router-link>
-                            </div>
+                                </div>
+                            </router-link>
                         </div>
                     </div>
                 </div>
             </div>
-        </transition>
+        </div>
     </div>
 </template>
 
